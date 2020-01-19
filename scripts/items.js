8e83a2fcc0bf6c5534cb4a2443ed06be67bcf435
Tinos Psomadakis <tinosps@gmail.com>
2020-01-19 22:03:58 +0000

    Hello World

--- scripts/items.js
+++ scripts/items.js
@@ -1,11 +1,11 @@
 var items = {
-    "banana": 120,
-    "lightbulb": 34,
-    "gold bar": 12400,
-    "African bush elephant": 6000000,
-    "cow": 1100000,
-    "earth": 5.972e24,
-    "Tesla model S": 2250000
+    "banana": [120, 2],
+    "lightbulb": [34, 2],
+    "gold bar": [12400, 2],
+    "African bush elephant": [6000000, 4],
+    "cow": [1100000, 4],
+    "earth": [5.972e24, 4],
+    "Tesla model S": [2250000, 4]
 
 
 };
