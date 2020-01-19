53b744612d174210ae6fff8ba6bd885b7d18c0e8
Tinos Psomadakis <tinosps@gmail.com>
2020-01-19 01:14:03 +0000

    Final for today

--- scripts/items.js
+++ scripts/items.js
@@ -1,5 +1,11 @@
 var items = {
-    "banana":120,
-    "lightbulb":34,
+    "banana": 120,
+    "lightbulb": 34,
+    "gold bar": 12400,
+    "African bush elephant": 6000000,
+    "cow": 1100000,
+    "earth": 5.972e24,
+    "Tesla model S": 2250000
+
 
 };
