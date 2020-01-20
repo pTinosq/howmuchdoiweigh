517a57ae7b3b7fee924382670773e49e12ebf2f6
tinos <tinosps@gmail.com>
2020-01-20 19:45:07 +0000

    more divs

--- scripts/items.js
+++ scripts/items.js
@@ -5,7 +5,9 @@ var items = {
     "African bush elephant": [6000000, 4],
     "cow": [1100000, 4],
     "earth": [5.972e24, 4],
-    "Tesla model S": [2250000, 4]
+    "Tesla model S": [2250000, 4],
+    "pencil": [8, 2],
+    "Cessna Skyhawk 172": [757045.70000302, 4]
 
 
 };
